e31149229008f0d9835707c46f8218d1ead459db
your_x500 <your_x500@umn.edu>
2017-11-06 15:21:44 -0800

    add temo so function

---
+++ R/so_function.R
@@ -0,0 +1,18 @@
+#' A Cat Function
+#'
+#' This function allows you to express your love of cats.
+#' @param love Do you love cats? Defaults to TRUE.
+#' @keywords cats
+#' @export
+#' @examples
+#' cat_function()
+
+library(dplyr)
+
+all_cust <- function(so,isvisit){
+  all_cust <- so%>%
+    mutate(luck = isvisit + 1)%>%
+    filter(deseasonedSales > isvisit)
+  
+  
+}
